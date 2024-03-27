922989556aac236cefff3577af2683a95bc2e083
Jakub <jakub.makowiec@outlook.com>
2024-03-27 21:37:29 +0100

    test

---
+++ src/main/java/poc/Client.java
@@ -0,0 +1,16 @@
+package poc;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Client {
+
+    private final String firstName;
+    private final String lastName;
+}
--- src/main/java/poc/XDocReportPOC.java
+++ src/main/java/poc/XDocReportPOC.java
@@ -1,10 +1,11 @@
 package poc;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 import fr.opensagres.xdocreport.core.XDocReportException;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
 import fr.opensagres.xdocreport.template.TemplateEngineKind;
 import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
 
@@ -12,19 +13,27 @@ public class XDocReportPOC {
 
     public static void main(String[] args) throws XDocReportException, IOException {
 
+        // 1) Load Docx file by filling Velocity template engine and cache
+        // it to the registry
+        InputStream in = XDocReportPOC.class.getResourceAsStream("./test.docx");
+
+        IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Velocity);
+
+        // 2) Create fields metadata to manage lazy loop (#forech velocity)
+        // for table row.
         // 1) Create FieldsMetadata by setting Velocity as template engine
-        FieldsMetadata fieldsMetadata = new FieldsMetadata(TemplateEngineKind.Velocity.name());
-
+        FieldsMetadata fieldsMetadata = report.createFieldsMetadata();
         // 2) Load fields metadata from Java Class
-        fieldsMetadata.load("project", Project.class);
-        // Here load is called with true because model is a list of Developer.
-        fieldsMetadata.load("developers", Developer.class, true);
+        fieldsMetadata.load("client", Client.class);
 
-        // 3) Generate XML fields in the file "project.fields.xml".
-        // Extension *.fields.xml is very important to use it with MS Macro XDocReport.dotm
-        // FieldsMetadata#saveXML is called with true to indent the XML.
-        File xmlFieldsFile = new File("project.fields.xml");
-        fieldsMetadata.saveXML(new FileOutputStream(xmlFieldsFile), true);
+        // 3) Create context Java model
+        IContext context = report.createContext();
+        Client client = new Client("Jan", "Kowalski");
+        context.put("client", client);
+
+        // 4) Generate report by merging Java model with the Docx
+        OutputStream out = new FileOutputStream(new File("project_out.docx"));
+        report.process(context, out);
     }
 
 }
