0809a70c6da5a77ef18a1d86e09621bbcae94b18
Jakub Makowiec <jakub.makowiec@comarch.com>
2024-03-27 20:09:05 +0100

    Initial commit

---
+++ src/main/java/poc/Project.java
@@ -0,0 +1,14 @@
+package poc;
+
+public class Project {
+
+    private final String name;
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
